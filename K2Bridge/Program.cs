a7374a65e5abea211973449204df319d825032ef
tamirkamara <26870601+tamirkamara@users.noreply.github.com>
2019-07-25 13:08:13 +0300

    config change

--- K2Bridge/Program.cs
+++ K2Bridge/Program.cs
@@ -1,15 +1,18 @@
 ﻿namespace K2Bridge
 {
     using Microsoft.Extensions.Configuration;
+    using System;
 
     public class Program
     {
         private const string ConfigFileName = "appsettings.json";
+        private const string LocalConfigFileName = "appsettings.local.json";
 
         public static void Main(string[] args)
         {
-            var config = new ConfigurationBuilder()
+            IConfigurationRoot config = new ConfigurationBuilder()
                 .AddJsonFile(ConfigFileName, false, true)
+                .AddJsonFile(LocalConfigFileName, true, true) // Optional for local development
                 .Build();
 
             Serilog.ILogger logger = Logger.GetLogger();
