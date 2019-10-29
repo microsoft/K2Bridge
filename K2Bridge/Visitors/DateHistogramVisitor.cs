670ce53026213e9f3f871b4512f39b54bad7b4de
chenhend <54312611+chenhend@users.noreply.github.com>
2019-10-29 12:15:24 +0200

    Added function to change Timespan (#17)
    * up to date with master and changing "KQLTimespan" to "ToKQLTimespan"
    * handle interval = "1w"/"1M/"1y"

--- K2Bridge/Visitors/DateHistogramVisitor.cs
+++ K2Bridge/Visitors/DateHistogramVisitor.cs
@@ -1,11 +1,39 @@
-﻿namespace K2Bridge.Visitors
+﻿using System.Linq;
+
+namespace K2Bridge.Visitors
 {
     internal partial class ElasticSearchDSLVisitor : IVisitor
     {
         public void Visit(Models.Request.Aggregations.DateHistogram dateHistogram)
         {
+            dateHistogram.KQL = $"{dateHistogram.Metric} by {dateHistogram.FieldName} = ";
+            if (!string.IsNullOrEmpty(dateHistogram.Interval))
+            {
+                var period = dateHistogram.Interval[dateHistogram.Interval.Length - 1];
+                switch (period)
+                {
+                    case 'w':
+                        dateHistogram.KQL += $"startofweek({dateHistogram.FieldName})";
+                        break;
+                    case 'M':
+                        dateHistogram.KQL += $"startofmonth({dateHistogram.FieldName})";
+                        break;
+                    case 'y':
+                        dateHistogram.KQL += $"startofyear({dateHistogram.FieldName})";
+                        break;
+                    default:
+                        // todatetime is redundent but we'll keep it for now
+                        dateHistogram.KQL += $"bin(todatetime({dateHistogram.FieldName}), {dateHistogram.Interval})";
+                        break;
+                }
+            }
+            else
+            {
+                dateHistogram.KQL += dateHistogram.FieldName;
+            }
+
             // todatetime is redundent but we'll keep it for now
-            dateHistogram.KQL = $"{dateHistogram.Metric} by bin(todatetime({dateHistogram.FieldName}), {dateHistogram.Interval}) | order by todatetime({dateHistogram.FieldName}) asc";
+            dateHistogram.KQL += $" | order by todatetime({dateHistogram.FieldName}) asc";
         }
     }
 }
