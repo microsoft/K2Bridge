e7491db7b0d29e2664a2c77e9fe4567fe35a8640
Elad Iwanir <13205761+eladiw@users.noreply.github.com>
2020-02-24 10:18:29 +0200

    test

--- K2Bridge.Tests.End2End/PopulateKusto.cs
+++ K2Bridge.Tests.End2End/PopulateKusto.cs
@@ -128,12 +128,12 @@ namespace K2Bridge.Tests.End2End
 
             // Send drop table ifexists command to Kusto
             var command = CslCommandGenerator.GenerateTableDropCommand(table, true);
-            await kustoAdminClient.ExecuteControlCommandAsync(db, command);
+            kustoAdminClient.ExecuteControlCommand(db, command);
 
             // Send create table command to Kusto
             command = $".create table {table} ({string.Join(", ", kustoColumns)})";
             Console.WriteLine(command);
-            await kustoAdminClient.ExecuteControlCommandAsync(db, command);
+            kustoAdminClient.ExecuteControlCommand(db, command);
 
             // Send create table mapping command to Kusto
             command = CslCommandGenerator.GenerateTableJsonMappingCreateCommand(
