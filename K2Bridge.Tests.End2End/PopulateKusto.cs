52e8da19f265208a0228d0436d9ae80528e99f6a
LizaShak <iliza@outlook.com>
2020-02-24 12:59:26 +0200

    fix style issues

--- K2Bridge.Tests.End2End/PopulateKusto.cs
+++ K2Bridge.Tests.End2End/PopulateKusto.cs
@@ -6,6 +6,7 @@ namespace K2Bridge.Tests.End2End
 {
     using System;
     using System.Collections.Generic;
+    using System.Data;
     using System.IO;
     using System.Threading.Tasks;
     using Kusto.Data;
@@ -109,7 +110,7 @@ namespace K2Bridge.Tests.End2End
         /// <param name="db">Kusto database.</param>
         /// <param name="table">Kusto table within database.</param>
         /// <returns>Bulk Insert operation result.</returns>
-        public static async Task<IKustoIngestionResult> PopulateTypesIndex(KustoConnectionStringBuilder kusto, string db, string table)
+        public static async Task<IDataReader> PopulateTypesIndex(KustoConnectionStringBuilder kusto, string db, string table)
         {
             // Build list of columns and mappings to provision Kusto
             var kustoColumns = new List<string>();
@@ -141,7 +142,7 @@ namespace K2Bridge.Tests.End2End
 
             // Populate Kusto
             command = ".append types_index <| print x = true, datetime('2020-02-23T07:22:29.1990163Z'), guid(74be27de-1e4e-49d9-b579-fe0b331d3642), int(17), long(17), real(0.3), 'string type', 30m, decimal(0.3), dynamic({'a':123, 'b':'hello'})";
-            await kustoAdminClient.ExecuteControlCommandAsync(db, command);
+            return await kustoAdminClient.ExecuteControlCommandAsync(db, command);
         }
 
         /// <summary>
