22ea73588dbb86b9325175e8a1c9b52e938c34a1
Elad Iwanir <13205761+eladiw@users.noreply.github.com>
2020-02-25 10:22:48 +0200

    Add missing tests for HttpResponseMessageResultsTests (#281)

---
+++ K2Bridge.Tests.UnitTests/HttpMessages/HttpResponseMessageResultsTests.cs
@@ -0,0 +1,142 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+
+namespace UnitTests.K2Bridge.HttpMessages
+{
+    using System;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using global::K2Bridge.HttpMessages;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Http.Features;
+    using Microsoft.Extensions.Primitives;
+    using NUnit.Framework;
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA1001:owns disposable field(s) but is not disposable", Justification = "No need to do this.")]
+    [TestFixture]
+    public class HttpResponseMessageResultsTests
+    {
+        private const string Reason = "A good reason";
+
+        private HttpResponseMessageResult httpResponseMessageResult;
+
+        private HttpResponseMessage httpResponseMessage;
+
+        [Test]
+        public void Ctor_InvalidArg_ThrowsArgumentNullException()
+        {
+            Assert.Throws(
+                Is.TypeOf<ArgumentNullException>()
+                 .And.Message.Contains("Value cannot be null."),
+                () => new HttpResponseMessageResult(null));
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "No need to do this.")]
+        [SetUp]
+        public void SetUp()
+        {
+            httpResponseMessage = new HttpResponseMessage
+            {
+                StatusCode = (System.Net.HttpStatusCode)200,
+                ReasonPhrase = Reason,
+            };
+
+            httpResponseMessage.Headers.Add("my-custom-header", "val 1");
+
+            httpResponseMessage.Content = new StringContent("Your response text");
+            httpResponseMessage.Content.Headers.Add("my-custom-content-header", "val 1");
+
+            httpResponseMessageResult = new HttpResponseMessageResult(httpResponseMessage);
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "No need to do this.")]
+        [Test]
+        public async Task ExecuteResultAsync_WithValidContext_Passes()
+        {
+            // The ActionContext will hold the copy result
+            var ac = new Microsoft.AspNetCore.Mvc.ActionContext
+            {
+                HttpContext = new DefaultHttpContext(),
+            };
+
+            // Execute
+            await httpResponseMessageResult.ExecuteResultAsync(ac);
+
+            var res = ac.HttpContext.Response;
+            Assert.AreEqual(200, res.StatusCode);
+
+            var responseFeature = ac.HttpContext.Features.Get<IHttpResponseFeature>();
+            Assert.AreEqual(Reason, responseFeature.ReasonPhrase);
+
+            res.Headers.TryGetValue("my-custom-header", out StringValues headerVal);
+            Assert.AreEqual(httpResponseMessage.Headers.GetValues("my-custom-header"), headerVal);
+
+            var val = httpResponseMessage.Content.Headers.GetValues("my-custom-content-header");
+            res.Headers.TryGetValue("my-custom-content-header", out StringValues contentHeaderVal);
+            Assert.AreEqual(val, contentHeaderVal);
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "No need to do this.")]
+        [Test]
+        public async Task ExecuteResultAsync_IgnoresTransferEncoding_Passes()
+        {
+            // The ActionContext will hold the copy result
+            var ac = new Microsoft.AspNetCore.Mvc.ActionContext
+            {
+                HttpContext = new DefaultHttpContext(),
+            };
+
+            // Set transfer encoding
+            httpResponseMessage.Headers.TransferEncodingChunked = true;
+
+            // recreate result object with updated message.
+            httpResponseMessageResult = new HttpResponseMessageResult(httpResponseMessage);
+
+            // Execute
+            await httpResponseMessageResult.ExecuteResultAsync(ac);
+
+            var res = ac.HttpContext.Response;
+            Assert.AreEqual(200, res.StatusCode);
+
+            var responseFeature = ac.HttpContext.Features.Get<IHttpResponseFeature>();
+            Assert.AreEqual(Reason, responseFeature.ReasonPhrase);
+
+            res.Headers.TryGetValue("my-custom-header", out StringValues headerVal);
+            Assert.AreEqual(httpResponseMessage.Headers.GetValues("my-custom-header"), headerVal);
+
+            var val = httpResponseMessage.Content.Headers.GetValues("my-custom-content-header");
+            res.Headers.TryGetValue("my-custom-content-header", out StringValues contentHeaderVal);
+            Assert.AreEqual(val, contentHeaderVal);
+
+            // Verify transfer encoding was ignored
+            res.Headers.TryGetValue("Transfer-Encoding", out StringValues transferEnc);
+            Assert.IsTrue(transferEnc.Count == 0);
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "No need to do this.")]
+        [Test]
+        public void ExecuteResultAsync_WithActionContextNoHttpContext_Throws()
+        {
+            // The ActionContext that SHOULD hold the copy result
+            var ac = new Microsoft.AspNetCore.Mvc.ActionContext();
+
+            // Execute
+            Assert.ThrowsAsync(
+                Is.TypeOf<ArgumentNullException>()
+                 .And.Message.EqualTo($"Response message can not be null (Parameter 'response')"),
+                async () => await httpResponseMessageResult.ExecuteResultAsync(ac));
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "No need to do this.")]
+        [Test]
+        public void ExecuteResultAsync_WithoutActionContext_Throws()
+        {
+            // Execute
+            Assert.ThrowsAsync(
+                Is.TypeOf<ArgumentNullException>()
+                 .And.Message.EqualTo($"Response message can not be null (Parameter 'response')"),
+                async () => await httpResponseMessageResult.ExecuteResultAsync(null));
+        }
+    }
+}
