327219b36ba36d0985240b135611992e8f96bfa5
Gleb Lesnikov <g.lesnikov@dodopizza.com>
2020-05-28 22:31:30 +0300

    remove unused variable

--- K2Bridge/KustoDAL/KustoDataAccess.cs
+++ K2Bridge/KustoDAL/KustoDataAccess.cs
@@ -142,11 +142,10 @@ namespace K2Bridge.KustoDAL
                     string dynamicQuery = $"{tableName} | {KustoQLOperators.Where} ingestion_time() > ago({MaxFieldCapsLookbackTime}) | {KustoQLOperators.Where} isnotempty({fieldCapabilityElement.Name}) | {KustoQLOperators.Sample} {MaxFieldCapsSampleSize} | {KustoQLOperators.Project} {MaxFieldCapsUniqueGetSchemaColumn}={fieldCapabilityElement.Name} | {KustoQLOperators.Evaluate} bag_unpack({MaxFieldCapsUniqueGetSchemaColumn}) | {KustoQLOperators.GetSchema} | {KustoQLOperators.Project} ColumnName=strcat('{fieldCapabilityElement.Name}','.',ColumnName),ColumnType=DataType";
 
                     var dynamicQueryData = new QueryData(dynamicQuery, tableName, null, null);
-                    TimeSpan timeTaken;
                     IDataReader reader;
                     try
                     {
-                        (timeTaken, reader) = await Kusto.ExecuteQueryAsync(dynamicQueryData, RequestContext);
+                        (_, reader) = await Kusto.ExecuteQueryAsync(dynamicQueryData, RequestContext);
                     }
                     catch (K2Bridge.KustoDAL.QueryException)
                     {
