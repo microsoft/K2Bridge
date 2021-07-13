f1b0102c199f6f153dacd1523c437a2c40f06994
AsafMah <asafmahlev@microsoft.com>
2021-07-13 13:48:49 +0300

    Upgrade to latest sdk (#56)
    * Upgrade to latest sdk
    
    * Updated test to pass in latest version

--- K2Bridge.Tests.End2End/QueryControllerErrorsTests.cs
+++ K2Bridge.Tests.End2End/QueryControllerErrorsTests.cs
@@ -12,7 +12,26 @@ namespace K2Bridge.Tests.End2End
 
     public class QueryControllerErrorsTests : KustoTestBase
     {
-        private const string ExpectedInvalidQueryResponse = @"{""responses"":[{""error"":{""root_cause"":[{""type"":""SemanticException"",""reason"":""Semanticerror:'let_data=database(\""build7865\"").kibana_sample_data_flights|where(dayOfWehas\""1\"")and(timestamp>=unixtime_milliseconds_todatetime(1517954400000)andtimestamp<=unixtime_milliseconds_todatetime(1518127199999));(_data|summarizecount()bytimestamp=startofmonth(timestamp)|orderbytimestampasc|asaggs);(_data|orderbytimestampdesc|limit500|ashits)'hasthefollowingsemanticerror:'where'operator:Failedtoresolvescalarexpressionnamed'dayOfWe'."",""index_uuid"":""kibana_sample_data_flights"",""index"":""kibana_sample_data_flights""}],""type"":""QueryException"",""reason"":""Failedexecutingazuredataexplorerquery"",""phase"":""query""},""status"":500}]}";
+        private const string ExpectedInvalidQueryResponse = @"{
+  ""responses"": [
+        {
+            ""error"": {
+                ""root_cause"": [
+                {
+                    ""type"": ""SemanticException"",
+                    ""reason"": ""Semantic error: 'where' operator: Failed to resolve scalar expression named 'dayOfWe'. Query: 'let _data = database(\""build7865\"").kibana_sample_data_flights | where (dayOfWe has \""1\"") and (timestamp >= unixtime_milliseconds_todatetime(1517954400000) and timestamp <= unixtime_milliseconds_todatetime(1518127199999));\n(_data | summarize count() by timestamp = startofmonth(timestamp)\n| order by timestamp asc | as aggs);\n(_data | order by timestamp desc | limit 500 | as hits)'"",
+                    ""index_uuid"": ""kibana_sample_data_flights"",
+                    ""index"": ""kibana_sample_data_flights""
+                }
+                ],
+                ""type"": ""QueryException"",
+                ""reason"": ""Failed executing azure data explorer query"",
+                ""phase"": ""query""
+            },
+            ""status"": 500
+        }
+        ]
+    }";
 
         [Test]
         [Description("Search with invalid query (wrong column name) returns an elastic error response")]
@@ -26,6 +45,7 @@ namespace K2Bridge.Tests.End2End
             s.
             Replace("\\n", string.Empty, StringComparison.OrdinalIgnoreCase).
             Replace(" ", string.Empty, StringComparison.OrdinalIgnoreCase).
+            Replace("\r\n", string.Empty, StringComparison.OrdinalIgnoreCase).
             Replace("\n", string.Empty, StringComparison.OrdinalIgnoreCase);
 
         private void AssertJson(string expectedJsonString, JToken json)
@@ -35,8 +55,10 @@ namespace K2Bridge.Tests.End2End
             var assertString = NormalizeChars(json.ToString());
 
             // remove database name (specific to build) from json
-            expectedJsonString = Regex.Replace(expectedJsonString, "build\\d+", "database");
-            assertString = Regex.Replace(assertString, "build\\d+", "database");
+            var databasePattern = new Regex(@"database\(\\""[\w]+\\""\)"); // Matches database(\\"build7865\\")
+            var replacement = "database(\"A\")";
+            expectedJsonString = databasePattern.Replace(expectedJsonString, replacement);
+            assertString = databasePattern.Replace(assertString, replacement);
             Assert.AreEqual(
                 assertString,
                 expectedJsonString,
