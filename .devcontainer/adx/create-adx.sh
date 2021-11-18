8f1ad7e038181e9817a864fefc393f2770929f8e
dupuyjs <jdupuy@microsoft.com>
2021-11-18 15:56:28 +0000

    Add reminder to delete sp (and update color usage)

--- .devcontainer/adx/create-adx.sh
+++ .devcontainer/adx/create-adx.sh
@@ -42,12 +42,17 @@ STORAGE_ACCOUNT_NAME="stk2$unique"
 STORAGE_CONTAINER_NAME="scripts"
 SERVICE_PRINCIPAL_NAME="sp-k2-$unique"
 
+# Colors
+COLOR_GREEN=$(tput setaf 2)
+COLOR_YELLOW=$(tput setaf 3)
+COLOR_WHITE=$(tput setaf 7)
+
 # Create Resource Group
 RESOURCE_GROUP_SCOPE=$(az group create --location $LOCATION --resource-group $RESOURCE_GROUP_NAME --query id -o tsv)
 
 # Create Azure Data Explorer Cluster
 az extension add -n kusto
-echo "$(tput setaf 2)[1/8] Creating Azure Data Explorer Cluster. Please be patient. Long running operation (10 minutes).$(tput setaf 7)"
+echo "${COLOR_GREEN}[1/8] Creating Azure Data Explorer Cluster. Please be patient. Long running operation (10 minutes).${COLOR_WHITE}"
 az kusto cluster create \
     --cluster-name $ADX_CLUSTER_NAME \
     --sku name="Dev(No SLA)_Standard_D11_v2" tier="Basic" capacity=1 \
@@ -57,7 +62,7 @@ az kusto cluster create \
     --type="None" # Argument added to fix issue https://github.com/Azure/azure-cli/issues/18827
 
 # Create Azure Data Explorer Database
-echo "$(tput setaf 2)[2/8] Creating Azure Data Explorer Database.$(tput setaf 7)"
+echo "${COLOR_GREEN}[2/8] Creating Azure Data Explorer Database.${COLOR_WHITE}"
 az kusto database create \
     --cluster-name $ADX_CLUSTER_NAME \
     --database-name $ADX_DB_NAME \
@@ -65,17 +70,17 @@ az kusto database create \
     --read-write-database soft-delete-period=P365D hot-cache-period=P31D location=$LOCATION
 
 # Create Service Principal
-echo "$(tput setaf 2)[3/8] Creating Service Principal.$(tput setaf 7)"
+echo "${COLOR_GREEN}[3/8] Creating Service Principal.${COLOR_WHITE}"
 SECRETS=$(az ad sp create-for-rbac --name $SERVICE_PRINCIPAL_NAME --role Contributor --scopes $RESOURCE_GROUP_SCOPE)
 SERVICE_PRINCIPAL_FQN="aadapp=$(echo $SECRETS | jq -r '.appId');$(echo $SECRETS | jq -r '.tenant')"
 SERVICE_PRINCIPAL_DISPLAYNAME=$(echo $SECRETS | jq '.displayName')
 
 # Wait Service Principal propagation
-echo "$(tput setaf 2)[4/8] Wait 60 seconds until Service Principal is propagated.$(tput setaf 7)"
+echo "${COLOR_GREEN}[4/8] Wait 60 seconds until Service Principal is propagated.${COLOR_WHITE}"
 sleep 60
 
 # Assign Service Principal to database permissions
-echo "$(tput setaf 2)[5/8] Assigning database permissions.$(tput setaf 7)"
+echo "${COLOR_GREEN}[5/8] Assigning database permissions.${COLOR_WHITE}"
 az kusto database add-principal \
     --cluster-name $ADX_CLUSTER_NAME \
     --database-name $ADX_DB_NAME \
@@ -83,7 +88,7 @@ az kusto database add-principal \
     --value name=$SERVICE_PRINCIPAL_DISPLAYNAME fqn=$SERVICE_PRINCIPAL_FQN role="Viewer" type="App"
 
 # Create Storage Account
-echo "$(tput setaf 2)[6/8] Creating Storage Account.$(tput setaf 7)"
+echo "${COLOR_GREEN}[6/8] Creating Storage Account.${COLOR_WHITE}"
 az storage account create \
     --name $STORAGE_ACCOUNT_NAME \
     --location $LOCATION \
@@ -99,7 +104,7 @@ EXPIRY=$(date -u -d '1 hour' '+%Y-%m-%dT%H:%MZ')
 SAS=$(az storage container generate-sas --name $STORAGE_CONTAINER_NAME --permissions lr --expiry $EXPIRY --https-only -o tsv)
 
 # Create Azure Data Explorer Table
-echo "$(tput setaf 2)[7/8] Creating Azure Data Explorer Table.$(tput setaf 7)"
+echo "${COLOR_GREEN}[7/8] Creating Azure Data Explorer Table.${COLOR_WHITE}"
 az storage blob upload --container-name $STORAGE_CONTAINER_NAME --file table.kql
 
 az kusto script create \
@@ -112,7 +117,7 @@ az kusto script create \
     --force-update-tag $(cat /proc/sys/kernel/random/uuid)
 
 # Ingest sample data
-echo "$(tput setaf 2)[8/8] Ingesting Sample Data.$(tput setaf 7)"
+echo "${COLOR_GREEN}[8/8] Ingesting Sample Data.${COLOR_WHITE}"
 DATA_FILE_NAME=data.json.gz
 curl https://raw.githubusercontent.com/elastic/kibana/v6.8.5/src/server/sample_data/data_sets/flights/flights.json.gz --output $DATA_FILE_NAME
 
@@ -128,13 +133,17 @@ echo "export ELASTICSEARCH_URL=http://localhost:9200" >> ~/.bashrc
 echo "export KUSTO_URI=https://$ADX_CLUSTER_NAME.$LOCATION.kusto.windows.net" >> ~/.bashrc 
 echo "export KUSTO_DB=$ADX_DB_NAME" >> ~/.bashrc 
 
+# Reminder
+echo -e "\n"
+echo "${COLOR_YELLOW}At the end of your development, don't forget to delete service principal and its role assignements."
+echo "${COLOR_YELLOW}az ad sp delete --id $(echo $SECRETS | jq -r '.appId')"  
+
 # Variables needed to populate appsettings.development.json
-echo -e "$(tput setaf 2)\n\nUse following settings/secrets in appsettings.development.json:$(tput setaf 7)"
+echo -e "\n"
+echo "${COLOR_GREEN}Use following settings/secrets in appsettings.development.json:${COLOR_WHITE}"
 
-echo "aadClientId: $(tput setaf 2)$(echo $SECRETS | jq -r '.appId')$(tput setaf 7)"
-echo "aadClientSecret: $(tput setaf 2)$(echo $SECRETS | jq -r '.password')$(tput setaf 7)"
-echo "aadTenantId: $(tput setaf 2)$(echo $SECRETS | jq -r '.tenant')$(tput setaf 7)"
-echo "adxClusterUrl: $(tput setaf 2)https://$ADX_CLUSTER_NAME.$LOCATION.kusto.windows.net$(tput setaf 7)"
-echo "adxDefaultDatabaseName: $(tput setaf 2)$ADX_DB_NAME$(tput setaf 7)"
-
-
+echo "aadClientId: ${COLOR_GREEN}$(echo $SECRETS | jq -r '.appId')${COLOR_WHITE}"
+echo "aadClientSecret: ${COLOR_GREEN}$(echo $SECRETS | jq -r '.password')${COLOR_WHITE}"
+echo "aadTenantId: ${COLOR_GREEN}$(echo $SECRETS | jq -r '.tenant')${COLOR_WHITE}"
+echo "adxClusterUrl: ${COLOR_GREEN}https://$ADX_CLUSTER_NAME.$LOCATION.kusto.windows.net${COLOR_WHITE}"
+echo "adxDefaultDatabaseName: ${COLOR_GREEN}$ADX_DB_NAME${COLOR_WHITE}"
