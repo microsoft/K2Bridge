093ddc5d132be9eeadc87e5c7f7a2a4b6ed1d4d0
dupuyjs <jdupuy@microsoft.com>
2021-11-18 12:32:00 +0000

    Update script to pass location in arguments

--- .devcontainer/adx/create-adx.sh
+++ .devcontainer/adx/create-adx.sh
@@ -3,21 +3,23 @@
 # Display Help
 Help()
 {
-   echo "Create data explorer cluster for development purpose."
+   echo "Create Azure Data Explorer (ADX) cluster for development purpose."
    echo
-   echo "Syntax: create-adx.sh [-u|h]"
+   echo "Syntax: create-adx.sh [-u|l|h]"
    echo "options:"
    echo "u     Unique name used to create resources."
+   echo "l     Location name. Values from: az account list-locations."
    echo "h     Print this Help."
    echo
 }
 
-while getopts u:h flag;
+while getopts u:l:h flag;
 do
     case "${flag}" in
         h) Help
            exit;;
         u) unique=${OPTARG};;
+        l) location=${OPTARG};;
     esac
 done
 
@@ -26,8 +28,13 @@ if [ ! "$unique" ]; then
     exit;
 fi
 
+if [ ! "$location" ]; then
+    echo "Please add location argument."
+    exit;
+fi
+
 # Resource Names
-RESOURCE_GROUP_LOCATION="westeurope"
+LOCATION=$location
 RESOURCE_GROUP_NAME="rg-k2-$unique-dev"
 ADX_CLUSTER_NAME="adxk2$unique"
 ADX_DB_NAME="dev"
@@ -36,26 +43,26 @@ STORAGE_CONTAINER_NAME="scripts"
 SERVICE_PRINCIPAL_NAME="sp-k2-$unique"
 
 # Create Resource Group
-RESOURCE_GROUP_SCOPE=$(az group create --location $RESOURCE_GROUP_LOCATION --resource-group $RESOURCE_GROUP_NAME --query id -o tsv)
+RESOURCE_GROUP_SCOPE=$(az group create --location $LOCATION --resource-group $RESOURCE_GROUP_NAME --query id -o tsv)
 
-# Create Data Explorer Cluster
+# Create Azure Data Explorer Cluster
 az extension add -n kusto
-echo "$(tput setaf 2)[1/8] Creating Data Explorer Cluster. Please be patient. Long running operation (10 minutes).$(tput setaf 7)"
+echo "$(tput setaf 2)[1/8] Creating Azure Data Explorer Cluster. Please be patient. Long running operation (10 minutes).$(tput setaf 7)"
 az kusto cluster create \
     --cluster-name $ADX_CLUSTER_NAME \
     --sku name="Dev(No SLA)_Standard_D11_v2" tier="Basic" capacity=1 \
     --resource-group $RESOURCE_GROUP_NAME \
-    --location $RESOURCE_GROUP_LOCATION \
+    --location $LOCATION \
     --enable-streaming-ingest true \
-    --type="None"
+    --type="None" # Argument added to fix issue https://github.com/Azure/azure-cli/issues/18827
 
-# Create Data Explorer Database
-echo "$(tput setaf 2)[2/8] Creating Data Explorer Database.$(tput setaf 7)"
+# Create Azure Data Explorer Database
+echo "$(tput setaf 2)[2/8] Creating Azure Data Explorer Database.$(tput setaf 7)"
 az kusto database create \
     --cluster-name $ADX_CLUSTER_NAME \
     --database-name $ADX_DB_NAME \
     --resource-group $RESOURCE_GROUP_NAME \
-    --read-write-database soft-delete-period=P365D hot-cache-period=P31D location=$RESOURCE_GROUP_LOCATION
+    --read-write-database soft-delete-period=P365D hot-cache-period=P31D location=$LOCATION
 
 # Create Service Principal
 echo "$(tput setaf 2)[3/8] Creating Service Principal.$(tput setaf 7)"
@@ -79,7 +86,7 @@ az kusto database add-principal \
 echo "$(tput setaf 2)[6/8] Creating Storage Account.$(tput setaf 7)"
 az storage account create \
     --name $STORAGE_ACCOUNT_NAME \
-    --location $RESOURCE_GROUP_LOCATION \
+    --location $LOCATION \
     --resource-group $RESOURCE_GROUP_NAME \
     --sku Standard_LRS
 
@@ -109,7 +116,7 @@ echo "$(tput setaf 2)[8/8] Ingesting Sample Data.$(tput setaf 7)"
 DATA_FILE_NAME=data.json.gz
 curl https://raw.githubusercontent.com/elastic/kibana/v6.8.5/src/server/sample_data/data_sets/flights/flights.json.gz --output $DATA_FILE_NAME
 
-KUSTO_URL=https://$ADX_CLUSTER_NAME.$RESOURCE_GROUP_LOCATION.kusto.windows.net
+KUSTO_URL=https://$ADX_CLUSTER_NAME.$LOCATION.kusto.windows.net
 TOKEN=$(az account get-access-token --scope "$KUSTO_URL/.default" -o tsv | cut -f1)
 
 curl -X POST "$KUSTO_URL/v1/rest/ingest/$ADX_DB_NAME/kibana_data_flights?streamFormat=json" --header "Authorization: Bearer $TOKEN" --header "Content-Encoding: gzip" --header "Content-Type: multipart/form-data" --data-binary "@$DATA_FILE_NAME"
@@ -118,7 +125,7 @@ rm $DATA_FILE_NAME
 # Environment variables needed to run end-to-end tests
 echo "export K2BRIDGE_URL=http://localhost:8080" >> ~/.bashrc 
 echo "export ELASTICSEARCH_URL=http://localhost:9200" >> ~/.bashrc 
-echo "export KUSTO_URI=https://$ADX_CLUSTER_NAME.$RESOURCE_GROUP_LOCATION.kusto.windows.net" >> ~/.bashrc 
+echo "export KUSTO_URI=https://$ADX_CLUSTER_NAME.$LOCATION.kusto.windows.net" >> ~/.bashrc 
 echo "export KUSTO_DB=$ADX_DB_NAME" >> ~/.bashrc 
 
 # Variables needed to populate appsettings.development.json
@@ -127,5 +134,5 @@ echo -e "$(tput setaf 2)\nUse following settings/secrets in appsettings.developm
 echo "aadClientId: $(tput setaf 2)$(echo $SECRETS | jq -r '.appId')$(tput setaf 7)"
 echo "aadClientSecret: $(tput setaf 2)$(echo $SECRETS | jq -r '.password')$(tput setaf 7)"
 echo "aadTenantId: $(tput setaf 2)$(echo $SECRETS | jq -r '.tenant')$(tput setaf 7)"
-echo "adxClusterUrl: $(tput setaf 2)https://$ADX_CLUSTER_NAME.$RESOURCE_GROUP_LOCATION.kusto.windows.net$(tput setaf 7)"
+echo "adxClusterUrl: $(tput setaf 2)https://$ADX_CLUSTER_NAME.$LOCATION.kusto.windows.net$(tput setaf 7)"
 echo "adxDefaultDatabaseName: $(tput setaf 2)$ADX_DB_NAME$(tput setaf 7)"
