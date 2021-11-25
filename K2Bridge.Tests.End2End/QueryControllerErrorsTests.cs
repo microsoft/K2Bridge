ae71e59ebd8e68c22ae54dc058a4dd36df24d65d
Thomas Conté <tconte@microsoft.com>
2021-11-25 09:58:14 +0000

    Fix query in error response

--- K2Bridge.Tests.End2End/QueryControllerErrorsTests.cs
+++ K2Bridge.Tests.End2End/QueryControllerErrorsTests.cs
@@ -19,7 +19,7 @@ namespace K2Bridge.Tests.End2End
                 ""root_cause"": [
                 {
                     ""type"": ""SemanticException"",
-                    ""reason"": ""Semantic error: 'where' operator: Failed to resolve scalar expression named 'dayOfWe'. Query: 'let _data = database(""A"").kibana_sample_data_flights | where (timestamp >= unixtime_milliseconds_todatetime(1517954400000) and timestamp <= unixtime_milliseconds_todatetime(1518127199999)) and (dayOfWe has \""1\"");\n(_data | summarize count() by timestamp = startofmonth(timestamp)\n| order by timestamp asc | project-rename _2=timestamp | as aggs);\n(_data | order by timestamp desc | limit 500 | as hits)'"",
+                    ""reason"": ""Semantic error: 'where' operator: Failed to resolve scalar expression named 'dayOfWe'. Query: 'let _data = database(""A"").kibana_sample_data_flights | where (timestamp >= unixtime_milliseconds_todatetime(1517954400000) and timestamp <= unixtime_milliseconds_todatetime(1518127199999)) and (dayOfWe has \""1\"");\n(_data | summarize count() by _2 = startofmonth(timestamp)\n| order by _2 asc | as aggs);\n(_data | order by timestamp desc | limit 500 | as hits)'"",
                     ""index_uuid"": ""kibana_sample_data_flights"",
                     ""index"": ""kibana_sample_data_flights""
                 }
