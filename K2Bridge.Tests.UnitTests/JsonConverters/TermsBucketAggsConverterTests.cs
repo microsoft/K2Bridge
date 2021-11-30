3ead24988d034e277db8f6b37f99fbd3ee3bf415
Thomas Conté <tconte@microsoft.com>
2021-11-30 16:14:17 +0000

    Additional unit tests

---
+++ K2Bridge.Tests.UnitTests/JsonConverters/TermsBucketAggsConverterTests.cs
@@ -0,0 +1,36 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+
+namespace UnitTests.K2Bridge.JsonConverters
+{
+    using global::K2Bridge.Models.Response;
+    using global::K2Bridge.Models.Response.Metadata;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class TermsBucketAggsConverterTests
+    {
+        private const string ExpectedValidBucket = @"{
+            ""doc_count"": 502,
+            ""key"": ""IT""
+        }";
+
+        private static readonly TermsBucket ValidTermsBucket = new TermsBucket()
+        {
+            DocCount = 502,
+            Key = "IT",
+            Aggs = new System.Collections.Generic.Dictionary<string, System.Collections.Generic.List<double>>(),
+        };
+
+        private static readonly object[] FieldCapsTestCases = {
+            new TestCaseData(ExpectedValidBucket, ValidTermsBucket).SetName("JsonDeserialize_WithValidTermsBucket_DeserializedCorrectly"),
+        };
+
+        [TestCaseSource(nameof(FieldCapsTestCases))]
+        public void TestTermsBucketAggsConverter(string queryString, object expected)
+        {
+            ((TermsBucket)expected).AssertJson(queryString);
+        }
+    }
+}
