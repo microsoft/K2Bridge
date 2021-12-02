741cf9607ada2f271b139fc9ce0541b8aacb5b38
dupuyjs <jdupuy@microsoft.com>
2021-12-02 09:50:19 +0000

    Add back JsonDeserializeObject_WithAggregationNoFieldsAvg_DeserializedCorrectly unit test

--- K2Bridge.Tests.UnitTests/JsonConverters/AggregationConverterTests.cs
+++ K2Bridge.Tests.UnitTests/JsonConverters/AggregationConverterTests.cs
@@ -41,6 +41,15 @@ namespace UnitTests.K2Bridge.JsonConverters
                 }
             }}";
 
+        private const string AvgEmptyFieldsAggregation = @"
+            {""aggs"": { 
+                ""2"": {
+                    ""avg"" : { 
+                        ""nofield"" : ""metric"" 
+                    } 
+                }
+            }}";
+
         private const string SumAggregation = @"
             {""aggs"": { 
                 ""2"": {
@@ -109,6 +118,22 @@ namespace UnitTests.K2Bridge.JsonConverters
                 },
         };
 
+        private static readonly Aggregation ExpectedNoFieldsAvgAggregation = new Aggregation()
+        {
+            PrimaryAggregation = null,
+            SubAggregations = new Dictionary<string, Aggregation>
+                {
+                    { "2", new Aggregation() {
+                        PrimaryAggregation = new AvgAggregation {
+                            FieldAlias = "_2",
+                            FieldName = null,
+                            },
+                        SubAggregations = new Dictionary<string, Aggregation>(),
+                        }
+                    },
+                },
+        };
+
         private static readonly Aggregation ExpectedValidSumAggregation = new Aggregation()
         {
             PrimaryAggregation = null,
@@ -142,6 +167,7 @@ namespace UnitTests.K2Bridge.JsonConverters
             new TestCaseData(DateHistogramAggregation, ExpectedValidDateHistogramAggregation).SetName("JsonDeserializeObject_WithAggregationValidDateHistogram_DeserializedCorrectly"),
             new TestCaseData(CardinalityAggregation, ExpectedValidCardinalityAggregation).SetName("JsonDeserializeObject_WithAggregationValidCardinality_DeserializedCorrectly"),
             new TestCaseData(AvgAggregation, ExpectedValidAvgAggregation).SetName("JsonDeserializeObject_WithAggregationValidAvg_DeserializedCorrectly"),
+            new TestCaseData(AvgEmptyFieldsAggregation, ExpectedNoFieldsAvgAggregation).SetName("JsonDeserializeObject_WithAggregationNoFieldsAvg_DeserializedCorrectly"),
             new TestCaseData(SumAggregation, ExpectedValidSumAggregation).SetName("JsonDeserializeObject_WithAggregationValidSum_DeserializedCorrectly"),
             new TestCaseData(NoAggAggregation, ExpectedNoAggAggregation).SetName("JsonDeserializeObject_WithNoAgg_DeserializedCorrectly"),
         };
