56084ee29df4457d83e93f1815a2a2d70313a407
dupuyjs <jdupuy@microsoft.com>
2021-12-06 12:37:18 +0000

    BucketAggregation and MetricAggregation inherits form Aggregation (instead LeafAggregation)

--- K2Bridge/Models/Request/Aggregations/Metric/MetricAggregation.cs
+++ K2Bridge/Models/Request/Aggregations/Metric/MetricAggregation.cs
@@ -11,12 +11,18 @@ namespace K2Bridge.Models.Request.Aggregations
     /// that compute metrics based on values extracted from the documents that are being aggregated.
     /// The values are extracted from the fields of the document (using the field data).
     /// </summary>
-    internal abstract class MetricAggregation : LeafAggregation
+    internal abstract class MetricAggregation : Aggregation
     {
         /// <summary>
-        /// Gets or sets field value for metric computation.
+        /// Gets or sets the field on which to aggregate.
         /// </summary>
         [JsonProperty("field")]
-        public string FieldName { get; set; }
+        public string Field { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value to use when the aggregation finds a missing value in a document.
+        /// </summary>
+        [JsonProperty("missing")]
+        public double? Missing { get; set; }
     }
 }
