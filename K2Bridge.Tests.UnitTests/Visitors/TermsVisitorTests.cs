4190c97fa8f8a75491e8e8058165f703ce4596e3
Asaf Mahlev <asafmahlev@microsoft.com>
2021-12-19 14:03:49 +0200

    spaces

--- K2Bridge.Tests.UnitTests/Visitors/TermsVisitorTests.cs
+++ K2Bridge.Tests.UnitTests/Visitors/TermsVisitorTests.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license.
 // See LICENSE file in the project root for full license information.
 
-
 namespace UnitTests.K2Bridge.Visitors
 {
     using global::K2Bridge.Models.Request.Aggregations;
