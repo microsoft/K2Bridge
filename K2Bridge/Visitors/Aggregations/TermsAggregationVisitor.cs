22f6496984bab2bfbb701a57dd47c60b021db2d5
dupuyjs <jdupuy@microsoft.com>
2021-12-20 16:20:48 +0000

    Rename (and clean) TermsVisitor to TermsAggregationVisitor

---
+++ K2Bridge/Visitors/Aggregations/TermsAggregationVisitor.cs
@@ -0,0 +1,44 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+
+namespace K2Bridge.Visitors
+{
+    using System.Text;
+    using K2Bridge.Models.Request.Aggregations;
+    using K2Bridge.Models.Response;
+
+    /// <content>
+    /// A visitor for the <see cref="TermsAggregation"/> element.
+    /// </content>
+    internal partial class ElasticSearchDSLVisitor : IVisitor
+    {
+        /// <inheritdoc/>
+        public void Visit(TermsAggregation termsAggregation)
+        {
+            Ensure.IsNotNull(termsAggregation, nameof(TermsAggregation));
+            EnsureClause.StringIsNotNullOrEmpty(termsAggregation.Metric, nameof(TermsAggregation.Metric));
+            EnsureClause.StringIsNotNullOrEmpty(termsAggregation.Field, nameof(TermsAggregation.Field));
+
+            var query = new StringBuilder();
+
+            // Add main aggregation query (summarize)
+            // KQL ==> _data | summarize ['key1']=metric(field1), ['key2']=metric(field2), count() by ['key']=field
+            query.Append($"_data | {KustoQLOperators.Summarize} {termsAggregation.SubAggregationsKustoQL}{termsAggregation.Metric} by ['{termsAggregation.Key}'] = {termsAggregation.Field}");
+
+            // Add order by (alphabetical, count and custom)
+            var orderBy = termsAggregation.Order.SortField switch
+            {
+                "_key" => $"{KustoQLOperators.CommandSeparator}{KustoQLOperators.OrderBy} ['{termsAggregation.Key}'] {termsAggregation.Order.SortOrder}",
+                "_count" => $"{KustoQLOperators.CommandSeparator}{KustoQLOperators.OrderBy} {BucketColumnNames.Count} {termsAggregation.Order.SortOrder}",
+                _ => $"{KustoQLOperators.CommandSeparator}{KustoQLOperators.OrderBy} ['{termsAggregation.Order.SortField}'] {termsAggregation.Order.SortOrder}"
+            };
+            query.Append(orderBy);
+
+            // Add limit
+            query.Append($"{KustoQLOperators.CommandSeparator}{KustoQLOperators.Limit} {termsAggregation.Size}");
+
+            termsAggregation.KustoQL = query.ToString();
+        }
+    }
+}
