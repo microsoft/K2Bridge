5b77b71ebe4d095fb97b4d90e1a8bd9c36957561
dupuyjs <jdupuy@microsoft.com>
2021-12-20 17:42:35 +0000

    Update DateHistogramBucket

---
+++ K2Bridge/Models/Response/Aggregations/Bucket/DateHistogram/DateHistogramBucket.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+
+namespace K2Bridge.Models.Response.Aggregations
+{
+    /// <summary>
+    /// Describes date histogram bucket response element.
+    /// Key is a 64 bit number representing a timestamp in milliseconds-since-the-epoch.
+    /// </summary>
+    public class DateHistogramBucket : KeyedBucket<double>
+    {
+    }
+}
