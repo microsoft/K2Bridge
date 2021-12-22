b43e69b20433ea8f18a4d6414a7b5ef47041ca43
dupuyjs <jdupuy@microsoft.com>
2021-12-22 08:49:38 +0000

    Add TermsBucketConverter

--- K2Bridge/Models/Response/Aggregations/Bucket/Terms/TermsBucket.cs
+++ K2Bridge/Models/Response/Aggregations/Bucket/Terms/TermsBucket.cs
@@ -4,9 +4,13 @@
 
 namespace K2Bridge.Models.Response.Aggregations
 {
+    using Newtonsoft.Json;
+    using K2Bridge.JsonConverters;
+
     /// <summary>
     /// Describes terms bucket response element.
     /// </summary>
+    [JsonConverter(typeof(TermsBucketConverter))]
     public class TermsBucket : KeyedBucket<string>
     {
     }
