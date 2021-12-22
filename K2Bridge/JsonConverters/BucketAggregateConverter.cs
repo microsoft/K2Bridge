f9a36cb35be1b2fcfb90bd7f313430cc59b02c77
dupuyjs <jdupuy@microsoft.com>
2021-12-22 16:27:50 +0000

    Add support for Keyed serialization.

---
+++ K2Bridge/JsonConverters/BucketAggregateConverter.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+
+namespace K2Bridge.JsonConverters
+{
+    using System.Linq;
+    using K2Bridge.JsonConverters.Base;
+    using K2Bridge.Models.Response.Aggregations;
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// This converter serializes <see cref="BucketAggregate"/> to Elasticsearh response json format.
+    /// </summary>
+    internal class BucketAggregateConverter : WriteOnlyJsonConverter
+    {
+        /// <inheritdoc/>
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteStartObject();
+            writer.WritePropertyName("buckets");
+
+            if (value is BucketAggregate<RangeBucket> rangeAggregate)
+            {
+                KeyedSerialization<RangeBucket, string>(writer, rangeAggregate, serializer);
+            }
+
+            if (value is BucketAggregate<DateHistogramBucket> dateHistogramAggregate)
+            {
+                serializer.Serialize(writer, dateHistogramAggregate.Buckets);
+            }
+
+            if (value is BucketAggregate<TermsBucket> termsAggregate)
+            {
+                serializer.Serialize(writer, termsAggregate.Buckets);
+            }
+
+            writer.WriteEndObject();
+        }
+
+        private void KeyedSerialization<TBucket, TKey>(JsonWriter writer, BucketAggregate<TBucket> aggregate, JsonSerializer serializer)
+            where TBucket : KeyedBucket<TKey>
+        {
+            // Setting the keyed flag to true will associate a unique string key with each bucket and return the ranges as a hash rather than an array.
+            if (aggregate.Keyed)
+            {
+                serializer.Serialize(writer, aggregate.Buckets.ToDictionary(bucket => bucket.Key, bucket => { bucket.Key = default(TKey); return bucket; }));
+            }
+            else
+            {
+                serializer.Serialize(writer, aggregate.Buckets);
+            }
+        }
+    }
+}
--- K2Bridge/Models/Response/Aggregations/BucketAggregate.cs
+++ K2Bridge/Models/Response/Aggregations/BucketAggregate.cs
@@ -5,18 +5,24 @@
 namespace K2Bridge.Models.Response.Aggregations
 {
     using System.Collections.Generic;
+    using K2Bridge.JsonConverters;
     using Newtonsoft.Json;
 
     /// <summary>
     /// Describes bucket aggregate response element.
     /// </summary>
+    [JsonConverter(typeof(BucketAggregateConverter))]
     public class BucketAggregate<TBucket> : IAggregate
         where TBucket : IBucket
     {
         /// <summary>
         /// Gets or sets a collection of buckets.
         /// </summary>
-        [JsonProperty("buckets")]
         public List<TBucket> Buckets { get; } = new List<TBucket>();
+
+        /// <summary>
+        /// Gets or sets the Keyed value.
+        /// </summary>
+        public bool Keyed { get; set; } = false;
     }
 }
