ebdb32aa0482ef2a84a9b4b8c50a25102bcd2e68
dupuyjs <jdupuy@microsoft.com>
2021-12-22 12:41:51 +0000

    Add DateHistogramBucketConverter

---
+++ K2Bridge/JsonConverters/DateHistogramBucketConverter.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+
+namespace K2Bridge.JsonConverters
+{
+    using System.Collections.Generic;
+    using K2Bridge.JsonConverters.Base;
+    using K2Bridge.Models.Response.Aggregations;
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// This converter serializes <see cref="DateHistogramBucket"/> to Elasticsearh response json format.
+    /// </summary>
+    internal class DateHistogramBucketConverter : WriteOnlyJsonConverter
+    {
+        /// <inheritdoc/>
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteStartObject();
+
+            if (value is DateHistogramBucket dateHistogramBucket)
+            {
+                writer.WritePropertyName("doc_count");
+                serializer.Serialize(writer, dateHistogramBucket.DocCount);
+
+                writer.WritePropertyName("key");
+                serializer.Serialize(writer, dateHistogramBucket.Key);
+
+                writer.WritePropertyName("key_as_string");
+                serializer.Serialize(writer, dateHistogramBucket.KeyAsString);
+
+                foreach (KeyValuePair<string, IAggregate> aggregate in dateHistogramBucket)
+                {
+                    writer.WritePropertyName(aggregate.Key);
+                    serializer.Serialize(writer, aggregate.Value);
+                }
+            }
+
+            writer.WriteEndObject();
+        }
+    }
+}
--- K2Bridge/Models/Response/Aggregations/Bucket/DateHistogram/DateHistogramBucket.cs
+++ K2Bridge/Models/Response/Aggregations/Bucket/DateHistogram/DateHistogramBucket.cs
@@ -4,10 +4,14 @@
 
 namespace K2Bridge.Models.Response.Aggregations
 {
+    using Newtonsoft.Json;
+    using K2Bridge.JsonConverters;
+
     /// <summary>
     /// Describes date histogram bucket response element.
     /// Key is a 64 bit number representing a timestamp in milliseconds-since-the-epoch.
     /// </summary>
+    [JsonConverter(typeof(DateHistogramBucketConverter))]
     public class DateHistogramBucket : KeyedBucket<double>
     {
     }
