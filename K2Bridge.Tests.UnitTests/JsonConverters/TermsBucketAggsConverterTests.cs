e7434e08b0dd2d56e0f746021ed96fd2374084f8
dupuyjs <jdupuy@microsoft.com>
2021-12-23 13:28:19 +0000

    Add new tests (code coverage).

--- K2Bridge.Tests.UnitTests/JsonConverters/TermsBucketAggsConverterTests.cs
+++ K2Bridge.Tests.UnitTests/JsonConverters/TermsBucketAggsConverterTests.cs
@@ -15,6 +15,12 @@ namespace UnitTests.K2Bridge.JsonConverters
             ""key"": ""IT""
         }";
 
+        private const string ExpectedValidBucketWithAggs = @"{
+            ""doc_count"": 502,
+            ""key"": ""IT"",
+            ""2"" : {""value"": 644.0861}
+        }";
+
         private static readonly TermsBucket ValidTermsBucket = new TermsBucket()
         {
             DocCount = 502,
@@ -23,7 +29,22 @@ namespace UnitTests.K2Bridge.JsonConverters
 
         private static readonly object[] FieldCapsTestCases = {
             new TestCaseData(ExpectedValidBucket, ValidTermsBucket).SetName("JsonDeserialize_WithValidTermsBucket_DeserializedCorrectly"),
+            new TestCaseData(ExpectedValidBucketWithAggs, ValidTermsBucketWithAggs).SetName("JsonDeserialize_WithValidTermsBucketWithAggs_DeserializedCorrectly"),
         };
+
+        private static TermsBucket ValidTermsBucketWithAggs
+        {
+            get
+            {
+                var bucket = new TermsBucket()
+                {
+                    DocCount = 502,
+                    Key = "IT",
+                };
+                bucket.Add("2", new ValueAggregate() { Value = 644.0861 });
+                return bucket;
+            }
+        }
 
         [TestCaseSource(nameof(FieldCapsTestCases))]
         public void TestTermsBucketAggsConverter(string queryString, object expected)
