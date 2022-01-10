cce62f8df24ee3073d2365c1f16cc56b0b0cf70b
Thomas Conté <tconte@microsoft.com>
2022-01-10 09:30:09 +0000

    Add tests for wildcards

--- K2Bridge.Tests.UnitTests/Visitors/FiltersVisitorTests.cs
+++ K2Bridge.Tests.UnitTests/Visitors/FiltersVisitorTests.cs
@@ -16,6 +16,8 @@ namespace UnitTests.K2Bridge.Visitors
     {
         [TestCase("foo:bar", "bar:baz", ExpectedResult = "_data | extend ['key'] = pack_array('foo:bar','bar:baz'), ['_filter_value'] = pack_array((foo has \"bar\"),(bar has \"baz\")) | mv-expand ['key'] to typeof(string), ['_filter_value'] | where ['_filter_value'] == true | summarize wibble by ['key'] | order by ['key'] asc")]
         [TestCase("foo:(bar OR baz)", "bar:(baz OR foo)", ExpectedResult = "_data | extend ['key'] = pack_array('foo:(bar OR baz)','bar:(baz OR foo)'), ['_filter_value'] = pack_array(((foo has \"bar\") or (foo has \"baz\")),((bar has \"baz\") or (bar has \"foo\"))) | mv-expand ['key'] to typeof(string), ['_filter_value'] | where ['_filter_value'] == true | summarize wibble by ['key'] | order by ['key'] asc")]
+        [TestCase("foo", "*", ExpectedResult = "_data | extend ['key'] = pack_array('foo','*'), ['_filter_value'] = pack_array((* has \"foo\"),(true)) | mv-expand ['key'] to typeof(string), ['_filter_value'] | where ['_filter_value'] == true | summarize wibble by ['key'] | order by ['key'] asc")]
+        [TestCase("foo", "bar:*", ExpectedResult = "_data | extend ['key'] = pack_array('foo','bar:*'), ['_filter_value'] = pack_array((* has \"foo\"),(bar matches regex \"(.)*\")) | mv-expand ['key'] to typeof(string), ['_filter_value'] | where ['_filter_value'] == true | summarize wibble by ['key'] | order by ['key'] asc")]
         public string FiltersVisit_WithAggregation_ReturnsValidResponse(string q1, string q2)
         {
             var rangeAggregation = new FiltersAggregation()
@@ -33,6 +35,8 @@ namespace UnitTests.K2Bridge.Visitors
                                 new QueryStringClause()
                                 {
                                     Phrase = q1,
+                                    Wildcard = true,
+                                    Default = "*",
                                 },
                             },
                         },
@@ -46,6 +50,8 @@ namespace UnitTests.K2Bridge.Visitors
                                 new QueryStringClause()
                                 {
                                     Phrase = q2,
+                                    Wildcard = true,
+                                    Default = "*",
                                 },
                             },
                         },
