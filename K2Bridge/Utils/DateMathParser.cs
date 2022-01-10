dc170bb45f608c8796cc56bf2580ccbb934975ed
Thomas Conté <tconte@microsoft.com>
2022-01-10 09:57:03 +0000

    New version of ParseDateMath

--- K2Bridge/Utils/DateMathParser.cs
+++ K2Bridge/Utils/DateMathParser.cs
@@ -4,6 +4,7 @@
 
 namespace K2Bridge.Visitors
 {
+    using System;
     using System.Text.RegularExpressions;
 
     /// <content>
@@ -14,68 +15,70 @@ namespace K2Bridge.Visitors
         public static string ParseDateMath(string expr)
         {
             // See Wiki or ES docs for details on date math syntax
-            var pattern = @"^(?<anchor>now|.+?(?:\|\||$))(?<ranges>(?:(?:\+|\-)[^\/]*))?(?<rounding>\/(?:y|M|w|d|h|H|m|s))?$";
-
-            Regex rgx = new Regex(pattern);
-            Match match = rgx.Match(expr);
+            var rgx = new Regex(
+                @"^
+                        (?: (?<anchor>now) | (?: (?<anchor>.+?) (?:\|\||$)) )
+                        (?: (?<rangeSign>[+-]) (?<rangeValue>\d+) (?<rangeUnit>[a-zA-Z]))?
+                        (?: (?:\/ (?<rounding>y|M|w|d|h|H|m|s)))?", RegexOptions.IgnorePatternWhitespace);
+            var match = rgx.Match(expr);
 
             var kexpr = string.Empty;
 
-            if (match.Success)
+            if (!match.Success)
             {
-                // The date literal, ie.g. YYYY-MM-DD or "now"
-                if (match.Groups["anchor"].Value.Equals("now"))
-                {
-                    kexpr = "now()";
-                }
-                else
-                {
-                    kexpr = $"make_datetime('{match.Groups["anchor"].Value.TrimEnd('|').TrimEnd('|')}')";
-                }
+                return kexpr;
+            }
 
-                // Date operation, e.g. -1M, +3d, etc.
-                if (match.Groups["ranges"].Success)
-                {
-                    var rangeStr = match.Groups["ranges"].Value;
-                    var valueStr = rangeStr.Substring(0, rangeStr.Length - 1);
-                    var unitStr = rangeStr[^1];
-                    var value = int.Parse(valueStr[0] == '+' ? valueStr.Substring(1) : valueStr);
-                    var unit = unitStr switch
-                    {
-                        'y' => "year",
-                        'M' => "month",
-                        'w' => "week",
-                        'd' => "day",
-                        'h' => "hour",
-                        'H' => "hour",
-                        'm' => "minute",
-                        's' => "second",
-                        _ => null,
-                    };
+            // The date literal, ie.g. YYYY-MM-DD or "now"
+            var anchor = match.Groups["anchor"].Value;
+            if (anchor.Equals("now"))
+            {
+                kexpr = "now()";
+            }
+            else
+            {
+                kexpr = $"make_datetime('{anchor}')";
+            }
 
-                    if (unit != null)
-                    {
-                        kexpr = $"datetime_add('{unit}', {value}, {kexpr})";
-                    }
-                }
-
-                // Rounding, e.g. /d, /h, /m, etc.
-                if (match.Groups["rounding"].Success)
+            // Date operation, e.g. -1M, +3d, etc.
+            if (match.Groups["rangeSign"].Success)
+            {
+                var sign = match.Groups["rangeSign"].Value == "-" ? "-" : string.Empty;
+                var value = int.Parse(match.Groups["rangeValue"].Value);
+                var unit = match.Groups["rangeUnit"].Value[0] switch
                 {
-                    var unit = match.Groups["rounding"].Value.Substring(1);
-                    kexpr = unit switch
-                    {
-                        "y" => $"startofyear({kexpr})",
-                        "M" => $"startofmonth({kexpr})",
-                        "w" => $"startofweek({kexpr})",
-                        "d" => $"startofday({kexpr})",
-                        "h" => $"bin({kexpr}, 1h)",
-                        "H" => $"bin({kexpr}, 1h)",
-                        "m" => $"bin({kexpr}, 1m)",
-                        "s" => $"bin({kexpr}, 1s)",
-                        _ => kexpr,
-                    };
+                    'y' => "year",
+                    'M' => "month",
+                    'w' => "week",
+                    'd' => "day",
+                    'h' => "hour",
+                    'H' => "hour",
+                    'm' => "minute",
+                    's' => "second",
+                    _ => throw new Exception("Invalid date math range unit."),
+                };
+
+                if (unit != null)
+                {
+                    kexpr = $"datetime_add('{unit}', {sign}{value}, {kexpr})";
                 }
+            }
+
+            // Rounding, e.g. /d, /h, /m, etc.
+            if (match.Groups["rounding"].Success)
+            {
+                kexpr = match.Groups["rounding"].Value switch
+                {
+                    "y" => $"startofyear({kexpr})",
+                    "M" => $"startofmonth({kexpr})",
+                    "w" => $"startofweek({kexpr})",
+                    "d" => $"startofday({kexpr})",
+                    "h" => $"bin({kexpr}, 1h)",
+                    "H" => $"bin({kexpr}, 1h)",
+                    "m" => $"bin({kexpr}, 1m)",
+                    "s" => $"bin({kexpr}, 1s)",
+                    _ => throw new Exception("Invalid date math rounding."),
+                };
             }
 
             return kexpr;
