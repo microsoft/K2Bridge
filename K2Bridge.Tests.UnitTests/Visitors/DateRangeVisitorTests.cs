0369afe338c3d01b7aab6344f83da5d3b4a10410
Thomas Conté <tconte@microsoft.com>
2022-01-10 10:03:10 +0000

    Use timestamp for unit test

--- K2Bridge.Tests.UnitTests/Visitors/DateRangeVisitorTests.cs
+++ K2Bridge.Tests.UnitTests/Visitors/DateRangeVisitorTests.cs
@@ -13,24 +13,24 @@ namespace UnitTests.K2Bridge.Visitors
     [TestFixture]
     public class DateRangeVisitorTests
     {
-        [TestCase("2018-02-01", "2018-02-02", "2018-02-02", "2018-02-3", ExpectedResult = "_data | extend ['key'] = pack_array(strcat(make_datetime('2018-02-01'), '%', make_datetime('2018-02-02')),strcat(make_datetime('2018-02-02'), '%', make_datetime('2018-02-3'))), ['_range_value'] = pack_array(['dayOfWeek'] >= make_datetime('2018-02-01') and ['dayOfWeek'] < make_datetime('2018-02-02'),['dayOfWeek'] >= make_datetime('2018-02-02') and ['dayOfWeek'] < make_datetime('2018-02-3')) | mv-expand ['key'] to typeof(string), ['_range_value'] | where ['_range_value'] == true | summarize wibble by ['key'] | order by ['key'] asc")]
-        [TestCase(null, "2018-02-02", "2018-02-02", "2018-02-3", ExpectedResult = "_data | extend ['key'] = pack_array(strcat('', '%', make_datetime('2018-02-02')),strcat(make_datetime('2018-02-02'), '%', make_datetime('2018-02-3'))), ['_range_value'] = pack_array(['dayOfWeek'] < make_datetime('2018-02-02'),['dayOfWeek'] >= make_datetime('2018-02-02') and ['dayOfWeek'] < make_datetime('2018-02-3')) | mv-expand ['key'] to typeof(string), ['_range_value'] | where ['_range_value'] == true | summarize wibble by ['key'] | order by ['key'] asc")]
-        [TestCase("2018-02-01", "2018-02-02", "2018-02-02", null, ExpectedResult = "_data | extend ['key'] = pack_array(strcat(make_datetime('2018-02-01'), '%', make_datetime('2018-02-02')),strcat(make_datetime('2018-02-02'), '%', '')), ['_range_value'] = pack_array(['dayOfWeek'] >= make_datetime('2018-02-01') and ['dayOfWeek'] < make_datetime('2018-02-02'),['dayOfWeek'] >= make_datetime('2018-02-02')) | mv-expand ['key'] to typeof(string), ['_range_value'] | where ['_range_value'] == true | summarize wibble by ['key'] | order by ['key'] asc")]
-        [TestCase(null, "2018-02-02", "2018-02-02", null, ExpectedResult = "_data | extend ['key'] = pack_array(strcat('', '%', make_datetime('2018-02-02')),strcat(make_datetime('2018-02-02'), '%', '')), ['_range_value'] = pack_array(['dayOfWeek'] < make_datetime('2018-02-02'),['dayOfWeek'] >= make_datetime('2018-02-02')) | mv-expand ['key'] to typeof(string), ['_range_value'] | where ['_range_value'] == true | summarize wibble by ['key'] | order by ['key'] asc")]
+        [TestCase("2018-02-01", "2018-02-02", "2018-02-02", "2018-02-3", ExpectedResult = "_data | extend ['key'] = pack_array(strcat(make_datetime('2018-02-01'), '%', make_datetime('2018-02-02')),strcat(make_datetime('2018-02-02'), '%', make_datetime('2018-02-3'))), ['_range_value'] = pack_array(['timestamp'] >= make_datetime('2018-02-01') and ['timestamp'] < make_datetime('2018-02-02'),['timestamp'] >= make_datetime('2018-02-02') and ['timestamp'] < make_datetime('2018-02-3')) | mv-expand ['key'] to typeof(string), ['_range_value'] | where ['_range_value'] == true | summarize wibble by ['key'] | order by ['key'] asc")]
+        [TestCase(null, "2018-02-02", "2018-02-02", "2018-02-3", ExpectedResult = "_data | extend ['key'] = pack_array(strcat('', '%', make_datetime('2018-02-02')),strcat(make_datetime('2018-02-02'), '%', make_datetime('2018-02-3'))), ['_range_value'] = pack_array(['timestamp'] < make_datetime('2018-02-02'),['timestamp'] >= make_datetime('2018-02-02') and ['timestamp'] < make_datetime('2018-02-3')) | mv-expand ['key'] to typeof(string), ['_range_value'] | where ['_range_value'] == true | summarize wibble by ['key'] | order by ['key'] asc")]
+        [TestCase("2018-02-01", "2018-02-02", "2018-02-02", null, ExpectedResult = "_data | extend ['key'] = pack_array(strcat(make_datetime('2018-02-01'), '%', make_datetime('2018-02-02')),strcat(make_datetime('2018-02-02'), '%', '')), ['_range_value'] = pack_array(['timestamp'] >= make_datetime('2018-02-01') and ['timestamp'] < make_datetime('2018-02-02'),['timestamp'] >= make_datetime('2018-02-02')) | mv-expand ['key'] to typeof(string), ['_range_value'] | where ['_range_value'] == true | summarize wibble by ['key'] | order by ['key'] asc")]
+        [TestCase(null, "2018-02-02", "2018-02-02", null, ExpectedResult = "_data | extend ['key'] = pack_array(strcat('', '%', make_datetime('2018-02-02')),strcat(make_datetime('2018-02-02'), '%', '')), ['_range_value'] = pack_array(['timestamp'] < make_datetime('2018-02-02'),['timestamp'] >= make_datetime('2018-02-02')) | mv-expand ['key'] to typeof(string), ['_range_value'] | where ['_range_value'] == true | summarize wibble by ['key'] | order by ['key'] asc")]
         public string DateRangeVisit_WithAggregation_ReturnsValidResponse(string from1, string to1, string from2, string to2)
         {
             var rangeAggregation = new DateRangeAggregation()
             {
                 Metric = "wibble",
-                Field = "dayOfWeek",
+                Field = "timestamp",
                 Key = "key",
                 Ranges = new List<DateRangeAggregationExpression>() {
-                    new DateRangeAggregationExpression { Field = "dayOfWeek", From = from1, To = to1 },
-                    new DateRangeAggregationExpression { Field = "dayOfWeek", From = from2, To = to2 },
+                    new DateRangeAggregationExpression { Field = "timestamp", From = from1, To = to1 },
+                    new DateRangeAggregationExpression { Field = "timestamp", From = from2, To = to2 },
                 },
             };
 
-            var visitor = new ElasticSearchDSLVisitor(SchemaRetrieverMock.CreateMockNumericSchemaRetriever());
+            var visitor = new ElasticSearchDSLVisitor(SchemaRetrieverMock.CreateMockTimestampSchemaRetriever());
             VisitorTestsUtils.VisitRootDsl(visitor);
             visitor.Visit(rangeAggregation);
 
