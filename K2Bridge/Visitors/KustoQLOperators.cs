172910b928b6d791ae0038e14504283cc92e5cdc
dupuyjs <jdupuy@microsoft.com>
2022-01-13 16:36:26 +0000

    Add ProjectAway Kusto Operator

--- K2Bridge/Visitors/KustoQLOperators.cs
+++ K2Bridge/Visitors/KustoQLOperators.cs
@@ -64,6 +64,7 @@ namespace K2Bridge.Visitors
         public const string Equal = "==";
         public const string Case = "case";
         public const string Extend = "extend";
+        public const string ProjectAway = "project-away";
         public const string ProjectReorder = "project-reorder";
         public const string Union = "union";
         public const string PackArray = "pack_array";
