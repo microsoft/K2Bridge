89c91fbf1636312408896db256003316a410459f
dupuyjs <jdupuy@microsoft.com>
2022-01-13 14:38:22 +0000

    Add ExtDataQuery query identifier

---
+++ K2Bridge/Utils/AggregationsSubQueries.cs
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+
+namespace K2Bridge.Utils
+{
+    public static class AggregationsSubQueries
+    {
+        /// <summary>
+        /// Query name used to identify extend data.
+        /// </summary>
+        public const string ExtDataQuery = "_extdata";
+    }
+}
