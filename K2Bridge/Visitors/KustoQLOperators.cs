7bd5268592e782eb33497db5baaf669a64ee29a7
dupuyjs <jdupuy@microsoft.com>
2022-01-13 14:37:25 +0000

    Add NewLine Kusto Operator

--- K2Bridge/Visitors/KustoQLOperators.cs
+++ K2Bridge/Visitors/KustoQLOperators.cs
@@ -69,6 +69,7 @@ namespace K2Bridge.Visitors
         public const string PackArray = "pack_array";
         public const string MvExpand = "mv-expand";
         public const string CommandSeparator = "\n| ";
+        public const string NewLine = "\n";
 #pragma warning restore SA1600 // Elements should be documented
     }
 }
