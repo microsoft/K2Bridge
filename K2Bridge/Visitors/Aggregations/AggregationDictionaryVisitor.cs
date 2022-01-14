177e3c416f3165293ea30928d42b402f34f4baa6
dupuyjs <jdupuy@microsoft.com>
2022-01-14 17:20:10 +0000

    Remove unused usings

--- K2Bridge/Visitors/Aggregations/AggregationDictionaryVisitor.cs
+++ K2Bridge/Visitors/Aggregations/AggregationDictionaryVisitor.cs
@@ -5,7 +5,6 @@
 namespace K2Bridge.Visitors
 {
     using System;
-    using System.Collections.Generic;
     using System.Linq;
     using System.Text;
     using K2Bridge.Models.Request.Aggregations;
--- K2Bridge/Visitors/Aggregations/DateRangeAggregationVisitor.cs
+++ K2Bridge/Visitors/Aggregations/DateRangeAggregationVisitor.cs
@@ -5,11 +5,8 @@
 namespace K2Bridge.Visitors
 {
     using System.Collections.Generic;
-    using System.Globalization;
     using System.Text;
     using K2Bridge.Models.Request.Aggregations;
-    using K2Bridge.Models.Response;
-    using K2Bridge.Utils;
 
     /// <content>
     /// A visitor for the <see cref="DateRangeAggregation"/> element.
