421755cf1f0443a9e2292ffca99d5b876dc4b421
Thomas Conté <199027+tomconte@users.noreply.github.com>
2022-01-17 14:07:27 +0100

    Handle missing buckets for Filters aggregation (#125)
    * Handle missing buckets for Filters aggregation
    
    * Comments, format
    
    * Added and updated unit tests

--- K2Bridge/Visitors/KustoQLOperators.cs
+++ K2Bridge/Visitors/KustoQLOperators.cs
@@ -65,6 +65,7 @@ namespace K2Bridge.Visitors
         public const string Case = "case";
         public const string Extend = "extend";
         public const string ProjectReorder = "project-reorder";
+        public const string ProjectRename = "project-rename";
         public const string Union = "union";
         public const string PackArray = "pack_array";
         public const string MvExpand = "mv-expand";
