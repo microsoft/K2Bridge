b467aaf7eec6890bde97d6bca6375a10a6d90ecf
Nebrass Lamouchi <lnibrass@gmail.com>
2022-01-18 07:58:40 +0000

    Change KustoQL Query

--- K2Bridge/Visitors/Aggregations/HistogramAggregationVisitor.cs
+++ K2Bridge/Visitors/Aggregations/HistogramAggregationVisitor.cs
@@ -12,9 +12,8 @@ namespace K2Bridge.Visitors
     /// <content>
     /// A visitor for the <see cref="HistogramAggregation"/> element.
     /// Sample KustoQL Query:
-    /// (_data
-    /// | summarize count() by ['2%False'] = bin(['AvgTicketPrice'], 20)
-    /// | where ['2%False'] between (50 .. 150)
+    /// | where ['AvgTicketPrice'] >= bin(50,20) and ['AvgTicketPrice'] < bin(150,20)+20
+    /// (_data | summarize count() by ['2%False'] = bin(['AvgTicketPrice'], 20)
     /// | where ['count_'] >= 1
     /// | order by ['2%False'] asc | as aggs);
     /// </content>
@@ -27,27 +26,28 @@ namespace K2Bridge.Visitors
             EnsureClause.StringIsNotNullOrEmpty(histogramAggregation.Metric, nameof(histogramAggregation.Metric));
             EnsureClause.StringIsNotNullOrEmpty(histogramAggregation.Field, nameof(histogramAggregation.Field));
 
-            var histogramKey = EncodeKustoField($"{histogramAggregation.Key}{AggregationsConstants.MetadataSeparator}{histogramAggregation.Keyed}");
-
-            histogramAggregation.KustoQL = $"{KustoTableNames.Data} ";
-
-            histogramAggregation.KustoQL += $"{KustoQLOperators.CommandSeparator}{KustoQLOperators.Summarize} {histogramAggregation.SubAggregationsKustoQL}" +
-            $"{histogramAggregation.Metric} by {histogramKey} = ";
-
             var interval = Convert.ToInt32(histogramAggregation.Interval);
             var field = EncodeKustoField(histogramAggregation.Field, true);
-
-            histogramAggregation.KustoQL += $"bin({field}, {interval})";
 
             if (histogramAggregation.HardBounds != null)
             {
                 var min = Convert.ToInt32(histogramAggregation.HardBounds.Min);
                 var max = Convert.ToInt32(histogramAggregation.HardBounds.Max);
-                histogramAggregation.KustoQL += $"{KustoQLOperators.CommandSeparator}{KustoQLOperators.Where} {histogramKey} between ({min} .. {max})";
+
+                histogramAggregation.KustoQL = $"{KustoQLOperators.CommandSeparator} {KustoQLOperators.Where} {field} >= bin({min}, {interval}) and {field} < bin({max}, {interval})+{interval}";
             }
 
-            histogramAggregation.KustoQL += $"{KustoQLOperators.CommandSeparator}{KustoQLOperators.Where} {EncodeKustoField(BucketColumnNames.Count)} >= {histogramAggregation.MinimumDocumentCount}";
-            histogramAggregation.KustoQL += $"{KustoQLOperators.CommandSeparator}{KustoQLOperators.OrderBy} {histogramKey} asc ";
+            histogramAggregation.KustoQL += $"{KustoQLOperators.NewLine}({KustoTableNames.Data} ";
+
+            var histogramKey = EncodeKustoField($"{histogramAggregation.Key}{AggregationsConstants.MetadataSeparator}{histogramAggregation.Keyed}");
+
+            histogramAggregation.KustoQL += $"{KustoQLOperators.CommandSeparator} {KustoQLOperators.Summarize} {histogramAggregation.SubAggregationsKustoQL}" +
+            $"{histogramAggregation.Metric} by {histogramKey} = ";
+
+            histogramAggregation.KustoQL += $"bin({field}, {interval})";
+
+            histogramAggregation.KustoQL += $"{KustoQLOperators.CommandSeparator} {KustoQLOperators.Where} {EncodeKustoField(BucketColumnNames.Count)} >= {histogramAggregation.MinimumDocumentCount}";
+            histogramAggregation.KustoQL += $"{KustoQLOperators.CommandSeparator} {KustoQLOperators.OrderBy} {histogramKey} asc ";
         }
     }
 }
