efda4ba341514766dea0a2c2eb8e8f99e0a48d81
AsafMah <asafmahlev@microsoft.com>
2022-12-19 13:11:02 +0200

    Update + eux

--- infrastructure/terraform/vnet/main.tf
+++ infrastructure/terraform/vnet/main.tf
@@ -12,9 +12,4 @@ resource "azurerm_subnet" "aks" {
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes       = ["10.100.1.0/24"]
-  # Avoid update after AKS sets up routing
-  # https://github.com/terraform-providers/terraform-provider-azurerm/issues/3749#issuecomment-532849895
-  lifecycle {
-    ignore_changes = [route_table_id]
-  }
 }
